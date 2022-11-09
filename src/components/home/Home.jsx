bbf016a0cbe3ed924a5394fd902f2fb68f099037
ricardograna <ricardogranarosales@gmail.com>
2022-11-10 00:23:57 +0100

    feat. background and video

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,22 @@
 import Socials from "../Socials/Socials"
+import "./Home.scss"
+import homeImg from "../../assets/frontopen.png"
+import ReactPlayer from "react-player"
 
 
 export const Home = () => {
   return (
-    <div>
+    <div className="home">
+    <img className="home-img" src={homeImg} alt="homeimg" />
         <Socials />
-
+       <iframe className="home-video" width="630" height="315" src="https://www.youtube.com/embed/FrE8-coyg2I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       <div>
+      <ReactPlayer
+        url="https://soundcloud.com/geerich"
+      />
     </div>
+    </div>
+
   )
 }
 
